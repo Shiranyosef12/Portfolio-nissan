eb7de3c14dc0e18ac251af17c127142c2a540b15
Shiranyosef12 <shirany.hit@gmail.com>
2023-06-24 23:26:37 +0300

    Initial commit

---
+++ test.js
@@ -0,0 +1,32 @@
+// function handleSubmit() {
+//     const name = document.getElementById("name").value;
+//     const surname = document.getElementById("surname").value;
+
+//     localStorage.setItem("NAME", name);
+//     localStorage.setItem("SURNAME", surname);
+
+//     return;
+// }
+
+
+// var submit = document.getElementById("submit");
+
+// submit.onclick = function() {
+//     var name = document.getElementById("name").value;
+//     var email = document.getElementById("email").value;
+//     var textarea = document.getElementById("textarea").value;
+
+
+//     document.getElementById("outputName").innerHTML = name;
+//     document.getElementById("outputEmail").innerHTML = email;
+//     document.getElementById("outputText").innerHTML = textarea;
+
+//     showData(name,email,textarea)
+// }
+
+// function showData(name,email,textarea) {
+//     alert(`${name}, ${email}, ${textarea}`);
+//     document.getElementById("outputName1").innerHTML = name;
+//     document.getElementById("outputEmail1").innerHTML = email;
+//     document.getElementById("outputText1").innerHTML = textarea;
+// }
